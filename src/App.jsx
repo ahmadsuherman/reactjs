3a3f98355b3c06603fa362d7b3981641bb3dbba6
ahmadsuherman <ahmad.223040066@mail.unpas.ac.id>
2024-11-22 13:46:47 +0700

    [Video 2] - Tutorial React JS Bahasa Indonesia : 2.Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+          <div>Ahmad Suherman</div>
+      </div>
+  )
+}
+
+export default App
