c55188a2f0bfaba75cfee75d8128644be1544f77
ahmadsuherman <ahmad.223040066@mail.unpas.ac.id>
2024-11-22 14:10:33 +0700

    [Video 3] - Tutorial React JS Bahasa Indonesia : 3.Component & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-function App() {
+const Button = (props) => {
+    const{ children, variant= "bg-black"} = props;
+    return(
+        <button
+            className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+            type="submit"
+        >
+            {children}
+        </button>
+    );
+};
 
+function App() {
   return (
       <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-          <div>Ahmad Suherman</div>
+          <div className="flex gap-x-3">
+              <Button variant="bg-red-700">Login</Button>
+              <Button variant="bg-slate-700">Register</Button>
+              <Button>Logout</Button>
+          </div>
       </div>
-  )
+  );
 }
 
 export default App
